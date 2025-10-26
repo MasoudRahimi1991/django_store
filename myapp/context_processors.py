165ebed1554228c7aa7aa137c6847e19286d4eaa
MasoudRahimi1991 <masoud.rahimi1991@yahoo.com>
2025-10-26 18:55:47 +0100

    Update context_processors.py

--- myapp/context_processors.py
+++ myapp/context_processors.py
@@ -1,14 +1,14 @@
 from .models import Category, Cart, CartItem
 
 def cart_count(request):
-    # تعداد آیتم‌های سبد خرید کاربر
+   
     cart_count = 0
     if request.user.is_authenticated:
         cart = Cart.objects.filter(user=request.user).first()
         if cart:
             cart_count = sum(ci.quantity for ci in CartItem.objects.filter(cart=cart))
 
-    # فقط کتگوری‌های فعال
+  
     categories = Category.objects.filter(is_enabled=True).order_by('name')
 
     return {
